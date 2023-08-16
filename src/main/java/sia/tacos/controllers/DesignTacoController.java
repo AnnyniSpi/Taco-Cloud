636e3af95142a300e3d333cb57868f6cd0a26fae
Anna <annaspi333@gmail.com>
2023-08-16 19:52:52 +0300

    Define POST method processTaco in DesignTacoController

--- src/main/java/sia/tacos/controllers/DesignTacoController.java
+++ src/main/java/sia/tacos/controllers/DesignTacoController.java
@@ -3,10 +3,7 @@ package sia.tacos.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import sia.tacos.model.Ingredient;
 import sia.tacos.model.Taco;
 import sia.tacos.model.TacoOrder;
@@ -57,6 +54,14 @@ public class DesignTacoController {
     @GetMapping
     public String showDesignForm(){
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder){
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+
+        return "redirect:/orders/current";
     }
 
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
