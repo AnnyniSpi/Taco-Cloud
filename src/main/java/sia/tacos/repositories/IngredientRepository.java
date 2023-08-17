eecdb7b964f5a10d0d9b492d1d0c0e658ecaf604
Anna <annaspi333@gmail.com>
2023-08-17 20:36:51 +0300

    Add IngredientRepository interface and Add JdbcIngredientRepository class

---
+++ src/main/java/sia/tacos/repositories/IngredientRepository.java
@@ -0,0 +1,14 @@
+package sia.tacos.repositories;
+
+import sia.tacos.model.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+
+    Optional<Ingredient> findById(String id);
+
+    Ingredient save(Ingredient ingredient);
+}
