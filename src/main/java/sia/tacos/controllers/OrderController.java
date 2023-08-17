aff363f2621d7230eeb764f654bfc9ac76854b60
Anna <annaspi333@gmail.com>
2023-08-17 12:56:11 +0300

    Define POST method in OrderController class and Add orderPorm.html

--- src/main/java/sia/tacos/controllers/OrderController.java
+++ src/main/java/sia/tacos/controllers/OrderController.java
@@ -3,10 +3,8 @@ package sia.tacos.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import sia.tacos.model.TacoOrder;
 
 @Slf4j
@@ -18,6 +16,14 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm(){
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus){
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
     }
 
 }
