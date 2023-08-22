6f707b236b3f435756f433dd98d4ab5333097aae
Anna <annaspi333@gmail.com>
2023-08-22 17:19:03 +0300

    Update OrderRepository

--- src/main/java/sia/tacos/repositories/OrderRepository.java
+++ src/main/java/sia/tacos/repositories/OrderRepository.java
@@ -3,8 +3,7 @@ package sia.tacos.repositories;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacos.model.TacoOrder;
 
-import java.util.Date;
-import java.util.List;
+import java.util.UUID;
 
-public interface OrderRepository  extends CrudRepository<TacoOrder, Long> {
+public interface OrderRepository  extends CrudRepository<TacoOrder, UUID> {
 }
