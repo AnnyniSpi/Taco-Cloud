67fa7a50c8f382c689fc7bac8bec6aed87bb9d99
Anna <annaspi333@gmail.com>
2023-08-23 17:59:55 +0300

    Add User Entity

--- src/main/java/sia/tacos/configs/SecurityConfig.java
+++ src/main/java/sia/tacos/configs/SecurityConfig.java
@@ -2,8 +2,17 @@ package sia.tacos.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +20,14 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        List<UserDetails> userList = new ArrayList<>();
+        userList.add(new User("buzz", encoder.encode("password"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+
+        return new InMemoryUserDetailsManager(userList);
     }
 }
