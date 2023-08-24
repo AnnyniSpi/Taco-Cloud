532307d41c4a9e968602e409839aec2ce8a3a955
Anna <annaspi333@gmail.com>
2023-08-24 23:12:22 +0300

    Refactoring

--- src/main/java/sia/tacos/repositories/OrderRepository.java
+++ src/main/java/sia/tacos/repositories/OrderRepository.java
@@ -3,8 +3,6 @@ package sia.tacos.repositories;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacos.model.TacoOrder;
 
-import java.util.Date;
-import java.util.List;
 
 public interface OrderRepository  extends CrudRepository<TacoOrder, Long> {
 }
